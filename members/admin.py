55a8a5f247082d31c06352b99f123dcae5903f40
Pawin Phakum <jesper666@gmail.com>
2019-05-09 01:45:22 +0700

    เพิ่ม m2m

--- members/admin.py
+++ members/admin.py
@@ -4,3 +4,5 @@ from .models import *
 # Register your models here.
 admin.site.register(Customer)
 admin.site.register(Favorite)
+admin.site.register(Pizza)
+admin.site.register(Topping)
--- members/models.py
+++ members/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+#----- ตัวอย่าง one-to-many
 class Customer(models.Model):
     name = models.CharField('ชื่อ นามสกุล', max_length=200)
     def __str__(self):
@@ -9,5 +11,21 @@ class Customer(models.Model):
 class Favorite(models.Model):
     name = models.CharField('หน้าพิซซ่าที่ชอบ', max_length=200)
     customer = models.ForeignKey(Customer, related_name='favorites', verbose_name='ลูกค้า', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
+#----- ตัวอย่าง many-to-many
+class Topping(models.Model):
+    name = models.CharField('ท็อปปิ้ง', max_length=200)
+    class Meta:
+        ordering = ('name',)
+    def __str__(self):
+        return self.name
+
+class Pizza(models.Model):
+    name = models.CharField('ชื่อเมนูพิซซ่า', max_length=200)
+    toppings = models.ManyToManyField(Topping)
+    class Meta:
+        ordering = ('name',)
     def __str__(self):
         return self.name
