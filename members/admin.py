df763c337a2c94aa64d0c8a590064395a4b923db
Pawin Phakum <jesper666@gmail.com>
2020-01-26 22:34:09 +0700

    admin search

--- members/admin.py
+++ members/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import *
 
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    ordering = ['-name']
+    search_fields = ['name']
+
 # Register your models here.
 admin.site.register(Customer)
-admin.site.register(Favorite)
 admin.site.register(Pizza)
 admin.site.register(Topping)
